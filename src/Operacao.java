503c63c982ccc1e1f553bf83691dd75d860241f4
Ana Luiza Diniz <anadiniz.ds@gmail.com>
2024-03-29 21:39:42 -0300

    ajustes na identação

--- src/Operacao.java
+++ src/Operacao.java
@@ -3,19 +3,16 @@ public class Operacao {
 
 
     // Testar se o grafo é simples,
-public static boolean grafosimples(int[][] matrizAdj) 
-   
-{
-
-
-    int numeroVertice = matrizAdj.length;
-    for (int i=0;i<numeroVertice ;i++)
-    {   for (int j=0;j<numeroVertice ;j++)
-             if (matrizAdj[i][j]>1 || (i==j && matrizAdj[i][i]==1))
-                  {  return false; } 
-    } 
+    public static boolean grafosimples(int[][] matrizAdj) 
+    {
+        int numeroVertice = matrizAdj.length;
+        for (int i=0;i<numeroVertice ;i++)
+        {   for (int j=0;j<numeroVertice ;j++)
+            if (matrizAdj[i][j]>1 || (i==j && matrizAdj[i][i]==1))
+                {  return false; } 
+        } 
     
-    return true; // Grafo é simples
+        return true; // Grafo é simples
 
     }
 
@@ -26,26 +23,24 @@ public static boolean grafosimples(int[][] matrizAdj)
 
     public static boolean grafoCompleto(int[][] matrizAdj+"RETORNO DO GRAFO SIMPLES") 
     {   
-        if (grafosimples=true)
-        { for (int i = 0; i < matrizAdj.length; i++) {
-        for (int j = 0; j < matrizAdj[i].length; j++) {
-            if (i != j && matrizAdj[i][j] != 1) {
-                return false;
+        if (grafosimples=true){
+            for (int i = 0; i < matrizAdj.length; i++) {
+                for (int j = 0; j < matrizAdj[i].length; j++) {
+                    if (i != j && matrizAdj[i][j] != 1) {
+                    return false;
+                }
             }
         }
-    }
-    return true;
-    } else {
-    return false; // Se o grafo não for simples, não pode ser completo
-    }
+        return true;
+        } else {
+            return false; // Se o grafo não for simples, não pode ser completo
+        }
     }
 
 
 
     //testar se é bipartido 
     //sera que da pra comitar 
-
-
-
-
 }
+
+
--- src/Vertice.java
+++ src/Vertice.java
@@ -2,40 +2,38 @@ import java.util.Scanner;
 
 public class Vertice {
     
-
-
     Scanner sc = new Scanner(System.in);
 
     //NOMEAÇÃO DOS VÉRTICES
     public void nome(int numeroVertice)
-{ 
- String []nomes = new String[numeroVertice];
-    int k=0;
-    while (k<numeroVertice) {
-    System.out.printf("Digite o nome do %d vértice , ele será associado ao numero %d",k,k);
-    nomes[k]=sc.nextLine();
-    k++;
+    { 
+        String []nomes = new String[numeroVertice];
+        int k=0;
+        while (k<numeroVertice) {
+            System.out.printf("Digite o nome do %d vértice , ele será associado ao numero %d",k,k);
+            nomes[k]=sc.nextLine();
+            k++;
+        }
     }
-}
 
-public void associação(String nomes[],int matrizadj[][] ) { 
+    public void associação(String nomes[],int matrizadj[][] ) { 
 
 
-}
+    }   
 
 
     //Identificação de vizinhança  QQQ
     public void Ident( int numeroVertice, int tipo)
-{ 
-    int controle=0;
-         for (int controle=0;MatrizAdj<numeroVertice;controle ++) {
-               if ( tipo==1 && MatrizAdj[][numeroVertice]==1)   // tipo nao direcionado 
-    {                    System.out.println(" +origem+ é vizinho de +destino" );
-                                             } 
+    { 
+        int controle=0;
+        for (controle=0;MatrizAdj<numeroVertice;controle ++) {
+            if ( tipo==1 && MatrizAdj[][numeroVertice]==1)   // tipo nao direcionado 
+            {   System.out.println(" +origem+ é vizinho de +destino" );
+            } 
     
-                                            }
+        }
 
-}
+    }
 
 
 
@@ -44,8 +42,6 @@ public void associação(String nomes[],int matrizadj[][] ) {
 
 
     
-}
-
 }
 
 
