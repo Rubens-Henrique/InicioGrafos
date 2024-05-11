110b41add1cfda110810b6e392b648bd028cf7a8
Rubens-Henrique <128429640+Rubens-Henrique@users.noreply.github.com>
2024-05-11 13:00:53 -0300

    Adicionando operações

--- src/MatrizAdjCriacao.java
+++ src/MatrizAdjCriacao.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 public class MatrizAdjCriacao {
     
@@ -114,4 +117,124 @@ public class MatrizAdjCriacao {
             System.out.println();
         }
 
-    } //comparaçãoes
+    } 
+
+
+    //Ordenação Topológica
+
+    public static void OrdenaTop()
+    {
+        int V= matrizadj.length;
+        int[] grauentrada = new int[V]; //Grau de entrada  do vértice 
+    
+
+        Queue<Integer> fila0 = new LinkedList<>(); //fila do grau de entrada 0
+        Queue<Integer> filaoodenacao = new LinkedList<>(); 
+
+        if (!ehDirecionado) //Verifica se o grafo é direcionado 
+        {   System.out.println("Erro. O grafo é não direcionado !");
+            return ;
+            }
+            else {   
+        // Calcula o grau de entrada de cada vértice somando todas as adjacencias que ENTRAM 
+         for (int i = 0; i < V; i++) {
+             for (int j = 0; j < V; j++) {
+                grauentrada[j] += matrizadj[i][j];
+                    }
+                }
+
+        //ADICIONANDO os vertices de entrada 0 no inicio da fila
+        for (int i=0;i<V;i++)
+        {   if(grauentrada[i]==0)
+            {    fila0.add(i);
+            }
+
+        } 
+        while(!fila0.isEmpty())
+        {  
+            int x= fila0.poll();
+            filaoodenacao.add(x);
+
+              // Atualiza os graus de entrada ( tira 1 a cada busca)
+              for (int i = 0; i < V; i++) {
+                if (matrizadj[x][i] == 1) {
+                    if (--grauentrada[i] == 0) {
+                        fila0.add(i);}    }
+                  
+                    }
+        }
+            // Verifica se o grafo possui um ciclo
+            if (filaoodenacao.size() != V ) {
+                System.out.println("Erro. O grafo possui um ciclo !");
+                return;
+            }
+    
+
+            // Imprime a ordem topológica
+            System.out.println("Ordem Topológica:");
+            for (int i : filaoodenacao)
+                System.out.print(i + " ");
+    }
+
+        
+    }
+
+
+
+    //busca em largura 
+    public static int BuscaLargura(int VerticeIniccio) {
+
+        int V= matrizadj.length;
+        boolean[] visitados = new boolean[V];
+        Queue<Integer> fila = new LinkedList<>();
+        fila.add(VerticeIniccio);
+        
+          
+        int busca=0;
+        for (int Todosvertices=0; Todosvertices<V; Todosvertices++)
+        {  
+            
+            if(!visitados[Todosvertices]) 
+        {   busca++;
+            fila.clear(); // Limpa a fila para uma nova busca em largura
+            fila.add(Todosvertices); // Adiciona o vértice inicial à fila
+            visitados[Todosvertices] = true; // Marca o vértice inicial como visitado
+    
+            System.out.printf("Busca n%d  :  ",busca);
+             while (!fila.isEmpty()) {
+            int vis = fila.poll();
+    
+            // Imprime o nó atual
+
+            System.out.print(vis+1 + " ");
+            
+            // Para cada vértice adjacente ao vértice atual
+            for (int i = 0; i < V; i++) {
+                if (matrizadj[vis][i] == 1 && !visitados[i]) {
+                    // Adiciona o vértice adjacente à fila
+                    fila.add(i);
+                    // Marca o vértice adjacente como visitado
+                    visitados[i] = true;
+                }
+            } 
+        }
+
+
+        System.out.println();
+        }
+    }
+        return busca;
+}
+
+
+public static void conexo() {
+    int VerticeInicio = 0; // Defina o vértice inicial para a busca em largura
+    int controle = BuscaLargura(VerticeInicio); // Chama a função BuscaLargura e atribui seu retorno à variável controle
+    if (controle > 1) {
+        System.out.println("O grafo é desconexo");
+    } else {
+        System.out.println("O grafo é conexo");
+    }
+}
+
+}
